a7b7893387fbc75e3627b6bb9524ad8b8eb600af
Dev Zero <litkaponii@gmail.com>
2026-03-20 21:47:38 -0500

    Agrego clase Producto con atributos, contructor,getter, setter y toString

--- inventario_consola/src/com/inventario/Producto.java
+++ inventario_consola/src/com/inventario/Producto.java
@@ -2,4 +2,70 @@ package com.inventario;
 
 public class Producto {
 
+	private String codigo;
+	private String nombre;
+	private double precio;
+	private int stock;
+	private String categoria;
+
+	public Producto(String codigo, String nombre, double precio, int stock, String categoria) {
+		
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stock = stock;
+		this.categoria = categoria;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+	    return "\nCódigo: " + codigo +
+	           "\nNombre: " + nombre +
+	           "\nPrecio: S/." + precio +
+	           "\nStock: " + stock +
+	           "\nCategoría: " + categoria;
+	}
+	
+	
+	
 }
