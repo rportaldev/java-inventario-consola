b9b9cc850c310960f867eef6590429e3444cf011
Dev Zero <litkaponii@gmail.com>
2026-03-21 17:18:18 -0500

    Agrego clase GestorInventario con todos los metodos

--- inventario_consola/src/com/inventario/GestorInventario.java
+++ inventario_consola/src/com/inventario/GestorInventario.java
@@ -1,5 +1,73 @@
 package com.inventario;
 
+import java.util.HashMap;
+
 public class GestorInventario {
 
+	private HashMap<String, Producto> inventario = new HashMap<>();
+	
+	//METODOS
+	
+	//REGISTRAR PRODUCTO
+	public boolean registrarProducto(Producto producto) {
+		
+		if(inventario.containsKey(producto.getCodigo())) {
+	        return false;
+	    }
+
+	    if(producto.getPrecio() <= 0) {
+	        return false;
+	    }
+
+	    if(producto.getStock() < 0) {
+	        return false;
+	    }
+
+	    inventario.put(producto.getCodigo(), producto);
+	    return true;
+	}
+	
+	//MOSTRAR INVENTARIO
+	public void mostrarInventario() {
+		
+		for(Producto p : inventario.values()) {
+			System.out.println(p); 
+		}
+	}
+	
+	//BUSCAR UN PRODUCTO
+	public Producto buscarProducto(String codigo) {
+		
+		return inventario.get(codigo);
+	}
+	
+	//ACTUALIZAR EL STOCK
+	public boolean actualizarStock(String codigo, int stock) {
+		
+		Producto producto = inventario.get(codigo);
+		
+		if(producto == null) {
+	        return false;
+	    }
+		
+		producto.setStock(stock);
+	    return true;
+	}
+	
+	//ELIMINAR UN PRODUCTO
+	public void eliminarProducto(String codigo) {
+		
+		inventario.remove(codigo);		
+			
+	}
+	
+	//PRODUCTOS BAJO EN STOCK
+	public void productoBajoStock() {
+		
+		for(Producto p : inventario.values()) {
+			if(p.getStock() < 5) {
+				System.out.println(p); 
+			}
+		}
+	}
 }
